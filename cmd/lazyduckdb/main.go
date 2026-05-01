f1bdc4427af917378c73b6e5000332ef6ce711d9
Sven Claesson <sven.claesson@hedinit.com>
2026-05-01 17:26:40 +0200

    release v0.1.9

--- cmd/lazyduckdb/main.go
+++ cmd/lazyduckdb/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/svenclaesson/lazyduckdb/internal/update"
 )
 
-var version = "0.1.8"
+var version = "0.1.9"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version and exit")
